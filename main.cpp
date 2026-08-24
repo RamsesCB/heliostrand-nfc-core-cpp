b49c15183290acca630a6f0783b190975877b3b8
RamsesCB <sergiocontrerasbernaola22@gmail.com>
2026-08-24 13:11:59 -0500

    feat: inicializar repositorio con rama main, archivo main.cpp y extensiones recomendadas

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+    // Inicialización del sistema
+    Serial.begin(115200);
+    Serial.println("Iniciando proyecto Arduino App Fundamentos...");
+}
+
+void loop() {
+    // Código principal en ejecución continua
+    delay(1000);
+}
